f37a0d9d93d610db6236d7dee449280a57984029
umma4135 <umma4135@gmail.com>
2022-09-03 00:36:04 -0400

    BootStrap

--- src/main/java/com/TestingBee1/BootStrap.java
+++ src/main/java/com/TestingBee1/BootStrap.java
@@ -1,9 +1,11 @@
 package com.TestingBee1;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -19,13 +21,24 @@ public class BootStrap {
 		driver.manage().timeouts().implicitlyWait(3000l, TimeUnit.SECONDS);
 		driver.get("https://www.jquery-az.com/boots/demo.php?ex=63.0_2");
 		driver.findElement(By.xpath("//span[@class='multiselect-selected-text']")).click();
+		List <WebElement> list = driver.findElements(By.xpath("//ul[@class='multiselect-container dropdown-menu']//li"));
 		
-		
-		
-		
-		
+		for (int i=0;i<list.size();i++) {
+			System.out.println(list.get(i).getText());}
+		for (int i=0;i<list.size();i++)
+			if (list.get(i).getAttribute("class").equalsIgnoreCase("active")){
+				System.out.println(list.get(i).getText());
+			}else {
+				list.get(i).click();	
+			
+			
+			
+					
+		}		
 		Thread.sleep(3000);
 		driver.quit();
-	}
 
+		
+
+}
 }
